c90651b94225b81ef6acc0d1e576a5239fba175e
Allan Yip <papatekken@gmail.com>
2022-01-06 22:00:41 +0800

    break down code from app.js into different modules

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ Allan Yip
 small project for nodeJS
 
 ## Phase 1
-Chatroom with Auth0 login - incompleted
+Chatroom with Auth0 login 
+## Phase 2 - incompleted
+Show login with the message
+## Phase 3 - incompleted
+without login still allowed to read message but cannot send message out
 
--- app.js
+++ app.js
@@ -1,197 +1,44 @@
+'use strict';
+
 // application dependencies
-var createError = require('http-errors');
 var express = require('express');
+var app = express();
+var dotenv = require('dotenv');
+var createError = require('http-errors');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser'); 
 var logger = require('morgan');
-var session = require('express-session');
 var passport = require('passport');
-var Auth0Strategy = require('passport-auth0');
 
 
 //application component
-var userInViews = require('./lib/middleware/userInViews');
+var sessionSetup = require('./lib/middleware/sessionSetup');
+var ioServer 	= require('./lib/middleware/ioServer')(app);
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
+var userInViews = require('./lib/middleware/userInViews');
 
+//unused component which reserved for future usage
+//var secured = require('./lib/middleware/secured');
 
-
-/**
- * Module dependencies.
- */
-var app = express();
-
-var debug = require('debug')('project-c:server');
-var http = require('http');
-var secured = require('./lib/middleware/secured');
-
-
-/**
- * Get port from environment and store in Express.
- */
-
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-
-
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
-
-
-/**
- * Setup socket.io
- */
-
-    const {Server} = require("socket.io");
-    const io = new Server(server);
-
-    io.on('connection', (socket) => {
-      console.log('a user connected');
-      socket.broadcast.emit('hi');
-      socket.on('chat message', (msg) => {
-
-            io.emit('chat message', msg);
-      });  
-      socket.on('disconnect', () => {
-        console.log('user disconnected');
-      });
-      
-    });
-
-
-
-
-// config express-session
-var sessionSetting = {
-  secret: 'justaExample',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true
-};
-
-
-//autho0 connection setup
-// Configure Passport to use Auth0
-var strategy = new Auth0Strategy(
-  {
-    domain: process.env.AUTH0_DOMAIN,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:
-      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
-  },
-  function (accessToken, refreshToken, extraParams, profile, done) {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
-    // extraParams.id_token has the JSON Web Token
-    // profile has all the information from the user
-    return done(null, profile);
-  }
-);
-
-passport.use(strategy);
-
-// You can use this section to keep a smaller payload
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function (user, done) {
-  done(null, user);
-});
-
-
-
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session(sessionSetting));
+
+// Middlewares
+
+app.use(sessionSetup);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(userInViews());
-//app.use(setupChatroom());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use('/', authRouter);
 app.use('/', indexRouter);
-
 
 
 // catch 404 and forward to error handler
